681c38b88e4e54a594010124e7e5eac87e514f5e
Vitalii <666tiger666@gmail.com>
2018-05-30 16:06:33 +0300

    add class ClassListIEnumerator

---
+++ ClassIEnumerable/ClassListIEnumerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassIEnumerable
+{
+    class ClassListIEnumerator:IEnumerator
+    {
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Current
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
