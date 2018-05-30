52721cd6403b66890de2da00c078af0b9385920f
Vitalii <666tiger666@gmail.com>
2018-05-30 14:54:59 +0300

    IEnumeratorIEnumerable Initial version

---
+++ TestIEnumerableIEnumerator/Books.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestIEnumerableIEnumerator
+{
+    class Books
+    {
+        public Books(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
---
+++ TestIEnumerableIEnumerator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestIEnumerableIEnumerable;
+
+namespace TestIEnumerableIEnumerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library();
+            foreach (Books book in library)
+            {
+                Console.WriteLine(book.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
