562483dfff92e3f761e09d718aced15ad4d157bf
Vitalii <666tiger666@gmail.com>
2018-05-30 15:22:10 +0300

    Delete test IenumerableIEnumerator

---
+++ TestIEnumerableIEnumerator/Library.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestIEnumerableIEnumerator;
+
+namespace TestIEnumerableIEnumerable
+{
+    class Library : IEnumerable
+    {
+        private Books[] books;
+
+        public Library()
+        {
+            books = new Books[]
+            {
+                new Books("Fathers and kids"), 
+                new Books("War and peace"), 
+                new Books("Evgenii Onegin")
+            };
+        }
+        public int Length { get { return books.Length; } }
+        public Books this[int index]
+        {
+            get { return books[index]; }
+            set { books[index] = value; }
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return books.GetEnumerator();
+        }
+    }
+}
